d88c65f5619ac5fe2fc88fac2a34f58c7da57db9
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-06 11:41:23 +0300

    fix:about side

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const closeButton = document.getElementById("close-button");
 const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 
-if (openButton && closeButton && navMenu) {
-  openButton.addEventListener("click", () => {
-    navMenu.classList.add("active");
-    openButton.style.display = "none";
-    closeButton.style.display = "flex";
-  });
 
-  closeButton.addEventListener("click", () => {
+
+openButton.addEventListener("click", () => {
+  navMenu.classList.add("active");
+  openButton.style.display = "none";
+  closeButton.style.display = "flex";
+});
+
+closeButton.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  closeButton.style.display = "none";
+  openButton.style.display = "flex";
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     navMenu.classList.remove("active");
     closeButton.style.display = "none";
     openButton.style.display = "flex";
   });
-
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      navMenu.classList.remove("active");
-      closeButton.style.display = "none";
-      openButton.style.display = "flex";
-    });
-  });
-}
+});
 
 const header = document.querySelector("header");
 const contactBtn = document.querySelector(".contact-btn");
